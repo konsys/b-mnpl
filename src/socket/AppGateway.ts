f97ce28d899834fb6ef382a30002ec214c399897
Konstantin Sysuev <konstantin.sysuev@stm-labs.ru>
2020-03-11 09:53:33 +0300

    dices

---
+++ src/socket/AppGateway.ts
@@ -0,0 +1,58 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+const random = (min: number, max: number) => {
+  return Math.ceil(min + Math.random() * (max - min));
+};
+let id = 0;
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('rollDices')
+  handleMessage(client: Socket, payload: string): void {
+    const dice1 = random(0, 6);
+    const dice2 = random(0, 6);
+    const res = {
+      code: 0,
+      data: {
+        id: id++,
+        events: [
+          {
+            type: 'rollDices',
+            userId: 429935,
+            dices: [dice1, dice2],
+            meanPosition: dice1 + dice2,
+            _id: 'CVD+HgfgAAo=',
+          },
+        ],
+      },
+    };
+    this.server.emit('msgToClient', res);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init: ' + server);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id} args: ${args}`);
+  }
+}

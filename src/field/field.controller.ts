f1e9a1f36ac840ffce2b5c5942c65463d5df72fc
Konstantin Sysuev <harry-run@yandex.ru>
2020-03-20 17:27:42 +0300

    dfbdgb

--- src/field/field.controller.ts
+++ src/field/field.controller.ts
@@ -5,6 +5,7 @@ import { boardFields } from 'src/entities/fields';
 export class FieldController {
   @Get()
   async getHello(): Promise<string> {
+    console.log(2424243234);
     // const fields = await this.appService.findAll();
     return JSON.stringify(boardFields);
   }
--- src/field/field.service.ts
+++ src/field/field.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Field } from 'src/entities/field.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class FieldService {}
+export class FieldService {
+  constructor(
+    @InjectRepository(Field)
+    private fieldRepository: Repository<Field>,
+  ) {}
+
+  async findAll(): Promise<Field[]> {
+    return this.fieldRepository.find();
+    // return Promise.reWsolve(['d']);
+  }
+}

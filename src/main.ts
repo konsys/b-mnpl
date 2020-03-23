1b532b0c3a3334b238c5eecf35522f6c0e6a2e04
Konstantin Sysuev <harry-run@yandex.ru>
2020-03-23 18:25:51 +0300

    dfngzfgn

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
+import csurf from 'csurf';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    // logger: ['error', 'warn'],
+  });
+  app
+    .enableCors
+    // { origin: 'localhost:3000', methods: ['GET', 'POST'] }
+    ();
   app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+  app.use(csurf({ cookie: true }));
 
   await app.listen(3001);
 }

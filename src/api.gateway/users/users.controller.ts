3e09eac192b62dcaecbb87d027f72812f34977cb
Konstantin Sysuev <harry-run@yandex.ru>
2020-07-09 20:00:48 +0300

    dgvedg

--- src/api.gateway/users/users.controller.ts
+++ src/api.gateway/users/users.controller.ts
@@ -16,8 +16,6 @@ export class UsersController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    // return req.user;
-
     return this.authService.login(req.user);
   }
 

3188c29f89662ce010e7d618c9dc383a29a974ef
Konstantin Sysuev <harry-run@yandex.ru>
2020-07-11 15:26:56 +0300

    ascascasc

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '5s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],

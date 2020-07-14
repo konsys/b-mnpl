b1f846788b813d7ed3f5b93cbfdffceccc3bc2cf
Konstantin Sysuev <harry-run@yandex.ru>
2020-07-14 21:03:48 +0300

    wefwf

--- src/api.gateway/board.message/board.message.service.ts
+++ src/api.gateway/board.message/board.message.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { BoardMessage } from 'src/types/Board/board.types';
-import { actionsStore } from 'src/stores/actions.store';
 import { IBoardEvent } from 'src/types/Board/board.types';
 import { actionTypeToEventAdapter } from 'src/socket.messages/outcome.messages';
 import { UsersService } from '../users/users.service';
 import { FieldsService } from '../fields/fields.service';
+import { ActionService } from '../action/action.service';
 
 @Injectable()
 export class BoardMessageService {
   constructor(
     private readonly usersClient: UsersService,
     private readonly fieldsClient: FieldsService,
+    private readonly actionService: ActionService,
   ) {}
 
   async createBoardMessage(): Promise<BoardMessage> {
-    const actionState = actionsStore.getState();
+    const actionState = await this.actionService.getActionStore('kkk');
 
     let event: IBoardEvent = {
       // Adapt from actionStore to send to client

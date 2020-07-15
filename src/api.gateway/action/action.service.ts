c0b46c7fd800f8e43a35e1d127fb1858c26b4ae5
Konstantin Sysuev <harry-run@yandex.ru>
2020-07-15 08:53:26 +0300

    wedfwef

--- src/api.gateway/action/action.service.ts
+++ src/api.gateway/action/action.service.ts
@@ -4,13 +4,12 @@ import {
   IncomeMessageType,
 } from 'src/types/Board/board.types';
 import { nanoid } from 'nanoid';
-import { setNewRoundEvent, setNewTurnEvent } from 'src/stores/board.store';
 import { UsersService } from '../users/users.service';
 import { FieldsService } from '../fields/fields.service';
-import { redis } from 'src/main';
 import { BOARD_PARAMS } from 'src/params/board.params';
 import { TransactionService } from '../transaction/transaction.service';
 import { StoreService } from './store.service';
+import { BoardService } from '../board/board.service';
 
 export interface ICurrentAction {
   action: OutcomeMessageType | IncomeMessageType;
@@ -25,13 +24,14 @@ export class ActionService {
     private readonly usersService: UsersService,
     private readonly fieldsService: FieldsService,
     private readonly transactionService: TransactionService,
+    private readonly boardService: BoardService,
     private readonly store: StoreService,
   ) {}
 
   async buyFieldModal(gameId: string) {
     const player = await this.usersService.getActingPlayer(gameId);
-    const action = await this.getActionStore(gameId);
-    await this.setActionStore(gameId, {
+    const action = await this.store.getActionStore(gameId);
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_CAN_BUY_MODAL,
       userId: player.userId,
       actionId: nanoid(4),
@@ -52,7 +52,7 @@ export class ActionService {
 
     // Decrease player`s money;
     const transactionId = nanoid(4);
-    await this.transactionService.setTransaction(gameId, {
+    await this.store.setTransaction(gameId, {
       sum,
       reason: `Купить ${field.name}`,
       toUserId: BOARD_PARAMS.BANK_PLAYER_ID,
@@ -63,70 +63,70 @@ export class ActionService {
   }
 
   async unJailModal(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_UN_JAIL_MODAL,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async payUnJailModal(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_UNJAIL_PAYING_MODAL,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async payTaxModal(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_TAX_PAYING_MODAL,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async rollDicesAction(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_ROLL_DICES_ACTION,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async rollDicesModal(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_ROLL_DICES_MODAL,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async startAuctionModal(gameId: string) {
-    await this.setActionStore(gameId, {
+    await this.store.setActionStore(gameId, {
       action: OutcomeMessageType.OUTCOME_AUCTION_MODAL,
       userId: (await this.usersService.getActingPlayer(gameId)).userId,
       actionId: nanoid(4),
-      moveId: ++(await this.getActionStore(gameId)).moveId,
+      moveId: ++(await this.store.getActionStore(gameId)).moveId,
     });
   }
 
   async switchPlayerTurn(gameId: string, unJail: boolean) {
     unJail = unJail ? unJail : false;
-    const store = await this.usersService.getPlayersStore(gameId);
+    const store = await this.store.getPlayersStore(gameId);
     const index = await this.usersService.getActingPlayerIndex(gameId);
     let player = await this.usersService.getActingPlayer(gameId);
     let nextIndex = index;
 
     if (index === 0) {
-      setNewRoundEvent();
+      await this.boardService.setNewRoundEvent(gameId);
     } else {
-      setNewTurnEvent();
+      await this.boardService.setNewTurnEvent(gameId);
     }
     // Set Next round
     nextIndex === 0 && (await this.fieldsService.mortgageNextRound(gameId));

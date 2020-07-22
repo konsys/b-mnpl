bd5b760dd000d99b8ffc984758d3dd566230b2a5
Konstantins <Sysuev>
2020-07-22 16:05:08 +0300

    sadcsd

--- src/api.gateway/game/game.module.ts
+++ src/api.gateway/game/game.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { Module } from '@nestjs/common';
+import { MsNames } from 'src/types/ms/ms.types';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: MsNames.ACTIONS,
+        transport: Transport.NATS,
+      },
+    ]),
+  ],
   controllers: [GameController],
-  providers: [GameService]
+  providers: [GameService],
+  exports: [GameService],
 })
 export class GameModule {}

70cb52d32738702407a53117f0c2db079f270cef
Konstantins <Sysuev>
2020-07-22 16:44:12 +0300

    werfwefwef

--- src/modules/socket/board.socket.ts
+++ src/modules/socket/board.socket.ts
@@ -4,7 +4,6 @@ import {
   Logger,
   UseInterceptors,
 } from '@nestjs/common';
-import { IErrorMessage, StoreService } from '../ms/action/store.service';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -14,13 +13,10 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-import { BOARD_PARAMS } from 'src/params/board.params';
 import { BoardMessageService } from '../ms/action/board.message.service';
-import { FieldType } from 'src/entities/board.fields.entity';
-import { FieldsService } from 'src/api.gateway/fields/fields.service';
+import { IErrorMessage } from '../ms/action/store.service';
 import { SocketActions } from 'src/types/game/game.types';
 import _ from 'lodash';
-import { subscriber } from 'src/main';
 
 @Injectable()
 @UseInterceptors(ClassSerializerInterceptor)
@@ -29,10 +25,7 @@ export class BoardSocket
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private logger: Logger = new Logger('BoardSocket');
 
-  constructor(
-    private readonly boardService: BoardMessageService,
-    private readonly store: StoreService,
-  ) {}
+  constructor(private readonly boardService: BoardMessageService) {}
 
   @WebSocketServer()
   public static socketServer: Server;
@@ -46,7 +39,6 @@ export class BoardSocket
 
   public async emitError(error: IErrorMessage) {
     BoardSocket.socketServer.emit(SocketActions.ERROR_MESSAGE, error);
-    await this.store.resetError('kkk');
   }
 
   afterInit(server: Server) {
